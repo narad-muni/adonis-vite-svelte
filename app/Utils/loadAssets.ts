c0b55fb55b28cbdcdee79dee98e5d633e1f0bdf3
narad-muni <saumillinux@gmail.com>
2023-02-02 13:45:56 +0530

    init

---
+++ app/Utils/loadAssets.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import fs from 'fs-extra';
+
+let assetsData: any = null;
+
+const loadAssets = async () => {
+  if (assetsData) {
+    return assetsData;
+  }
+
+  assetsData = { found: false, css: [], js: [] };
+  const manifestPath = path.resolve('./public/build/manifest.json');
+  try {
+    await fs.access(manifestPath);
+    const manifestText = await fs.readFile(manifestPath, 'utf-8');
+    const manifest = JSON.parse(manifestText);
+    assetsData.found = true;
+    const pageImports = manifest['index.html'];
+    if(pageImports) {
+    pageImports.css?.forEach((css) => assetsData.css.push(`/build/${css}`));
+    pageImports.imports?.forEach((imp) => assetsData.js.push(`/build/${manifest[imp].file}`));
+    assetsData.js?.push(`/build/${pageImports.file}`);
+    }
+  } catch (e) {
+    if (!e.errno || e.errno !== -4058) {
+      console.error(e);
+    }
+  }
+  return assetsData;
+};
+
+export default loadAssets;
